4f52fa237bc4d109ddf4a6cf270e9887f74fffe2
phu318 <baonguyen3181999@gmail.com>
2023-10-26 16:39:25 +0700

    tao lai repository

---
+++ mang/ar.js
@@ -0,0 +1,24 @@
+// let names = ["Phu" , "Quang" , "Hiep"];
+
+
+// names.push("UYEN");
+// console.log(names.pop());
+
+
+// names.unshift("KHOa");
+// names.shift();
+// console.log(names);
+
+
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+// fruits.splice(2, 0, "Lemon", "Kiwi");
+
+// console.log(fruits);
+
+
+// const fruits = ["Banana", "Orange", "Apple", "Mango", "Kiwi"];
+// fruits.splice(2, 2, "QUANG");
+
+
+// console.log(fruits);
